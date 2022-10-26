9379eac78e8ffe6fe931b054c00af1a47bcece52
Riddy366 <ridwanabukar851@gmail.com>
2022-10-26 12:39:47 +0200

    Oblig3

---
+++ Oblig2/src/main/java/LeapYear.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class LeapYear {
+
+        public static boolean isLeapYear(int year) {
+
+            if (year % 4 == 0 && year % 100 != 0) {
+                System.out.println(year);
+                return true;
+            }
+            else if (year % 400 == 0) {
+                System.out.println(year);
+                return true;
+            } else {
+                System.out.println(year);
+                return false;
+            }
+        }
+
+        public static void main(String[] args) {
+            List<Integer> data = Arrays.asList(1700, 1800, 1900, 2000, 2100);
+            for (Integer d : data) {
+                System.out.println(d.toString() + " : " + isLeapYear(d));
+            }
+        }
+    }
+
