e6ea49b64fedd694c1826b0abe2e4ed4c990d0aa
Riddy366 <113443292+Riddy366@users.noreply.github.com>
2022-10-31 15:02:55 +0100

    Update LeapYear.java

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -6,14 +6,14 @@ public class LeapYear {
         public static boolean isLeapYear(int year) {
 
             if (year % 4 == 0 && year % 100 != 0) {
-                System.out.println(year);
+                System.out.println(year + "- True");
                 return true;
             }
             else if (year % 400 == 0) {
-                System.out.println(year);
+                System.out.println(year + "- True");
                 return true;
             } else {
-                System.out.println(year);
+                System.out.println(year + "- False");
                 return false;
             }
         }
